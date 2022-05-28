7d7280570f374c7452abec09de514736fa379c60
pafable <philafable@gmail.com>
2022-05-28 15:35:51 -0400

    override zoom in vehicle for plane

--- app.groovy
+++ app.groovy
@@ -4,8 +4,9 @@ import com.phil.mobile.Plane
 Car c1 = new Car("Chevrolet", "Corvette C8", 195, 2022)
 Plane p1 = new Plane("Grumman", "F-14 Tomcat", 761, 1974)
 
-println Car.zoom()
+println "C8 goes ${c1.zoom()}"
+println "F-14 goes ${p1.zoom()}"
 println c1.fullName()
-println "Is this car fast: ${c1.isFast}"
 
+println "Is this car fast: ${c1.isFast}"
 println "Is this plane fast: ${p1.isFast}"
